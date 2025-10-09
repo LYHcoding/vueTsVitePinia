3b911553e57259bfebb81d2887d9806e18772f97
李元昊 <847800124@qq.com>
2025-10-09 18:15:57 +0800

    update note

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,9 @@
     <div class="toRef border">
       <a class="titleText">toRef:</a><br />
       <div>state:{{ state }}</div>
-      <button @click="change1">test1</button>&nbsp;
-      <button @click="change2">test2</button>
+      <button @click="change1">change1</button>&nbsp;
+
+      <!-- <button @click="change2">change2</button> -->
     </div>
     <hr />
   </div>
@@ -14,13 +15,17 @@
 import { reactive, toRef } from 'vue'
 
 // toRef 转化为响应式对象
+// 如果原始对象是非响应式的就不会更新视图,数据是会变的
+// 如果原始对象是响应式的就会更新视图,并且改变数据的
 const obj = {
   foo: 1,
-  bar: 2
+  bar: 1
 }
 const state = toRef(obj,'bar')
-const cahnge = () => {
+// bar 转化为响应式对象
+const change1 = () => {
   state.value++
+  console.log(obj, state);
 }
 </script>
 
